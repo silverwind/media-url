67bfd78bd9b5116a23d56c29b016de3d548c7d7b
silverwind <me@silverwind.io>
2023-03-15 23:35:32 +0100

    reduce again

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,24 @@ import {fileURLToPath} from 'node:url';
 
 export default {
   entry: {
-    "theme-arc-green": fileURLToPath(new URL('css/themes/theme-arc-green.css', import.meta.url)),
-    "theme-auto": fileURLToPath(new URL('css/themes/theme-auto.css', import.meta.url)),
+    dark: fileURLToPath(new URL('dark.css', import.meta.url)),
+    index: fileURLToPath(new URL('index.css', import.meta.url)),
   },
   output: {
     path: fileURLToPath(new URL('dist', import.meta.url)),
   },
   mode: "development",
-  devtool: false,
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       }
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[name].[contenthash:8].css',
     }),
   ],
 }
